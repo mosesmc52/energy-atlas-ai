59b60aa597d6c0c74c501f6d9df49a287aad99b4
Moses McCall <mosesmc52@gmail.com>
2026-04-10 10:20:31 -0400

    insert Google Analytics

--- public/custom.js
+++ public/custom.js
@@ -1,7 +1,31 @@
 (function () {
+  const GA_MEASUREMENT_ID = "G-4DQHVPWTF2";
+  const GA_SCRIPT_ID = "energy-atlas-ga-script";
   const LINK_ID = "energy-atlas-signin-link";
   const LINK_CLASS = "energy-atlas-signin-link";
   const SIGNIN_PATH = "/auth/signin/";
+
+  function ensureGoogleAnalytics() {
+    if (document.getElementById(GA_SCRIPT_ID) || typeof document.head === "undefined") {
+      return;
+    }
+
+    window.dataLayer = window.dataLayer || [];
+    window.gtag =
+      window.gtag ||
+      function gtag() {
+        window.dataLayer.push(arguments);
+      };
+
+    window.gtag("js", new Date());
+    window.gtag("config", GA_MEASUREMENT_ID);
+
+    const script = document.createElement("script");
+    script.id = GA_SCRIPT_ID;
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+    document.head.appendChild(script);
+  }
 
   function ensureSignInLink() {
     const header = document.querySelector("header");
@@ -29,6 +53,7 @@
 
   const observer = new MutationObserver(() => ensureSignInLink());
   observer.observe(document.documentElement, { childList: true, subtree: true });
+  ensureGoogleAnalytics();
   window.addEventListener("load", ensureSignInLink);
   ensureSignInLink();
 })();
